1ed851e29c634aefc92179e589890fdcdcdda3e9
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 16:01:29 -0300

    Add Atividade_n4_1.java with main method

---
+++ atividades/atividades_n4/Atividade_n4_1.java
@@ -0,0 +1,9 @@
+package atividades.atividade_n4;
+
+public class Atividade_n4_1 {
+    public static void main(String[] args) {
+        boolean isAluno = true;
+
+        System.out.println(isAluno);
+    }
+}

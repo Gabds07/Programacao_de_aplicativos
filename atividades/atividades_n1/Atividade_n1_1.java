d43006b04b23f06fb04b4b4aa525f26870f817e0
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 15:57:12 -0300

    Add Atividade_n1_1 class with main method

---
+++ atividades/atividades_n1/Atividade_n1_1.java
@@ -0,0 +1,9 @@
+package atividades.atividade_n1;
+
+public class Atividade_n1_1 {
+    public static void main() {
+        int valor = 3145;
+
+        System.out.println("O valor é: " + valor);
+    }    
+}

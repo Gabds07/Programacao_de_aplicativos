a00181facb4a0675bd0c5fb6cbeef711f1290a69
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 15:59:21 -0300

    Add Atividade_n2_1.java for decimal number input

---
+++ atividades/atividades_n2/Atividade_n2_1.java
@@ -0,0 +1,14 @@
+package atividades.atividade_n2;
+
+import java.util.Scanner;
+
+public class Atividade_n2_1 {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        double num;
+        System.out.println("Digite um número decimal: ");
+        num = scanner.nextDouble();
+
+        System.out.println("Seu número é: " + num);
+    }
+}

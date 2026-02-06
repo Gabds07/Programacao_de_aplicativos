e24c5ef2cb93e240bea6dbd7d1fa3f0a05e8463a
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 16:00:10 -0300

    Add Atividade_n3_1.java for user greeting

---
+++ atividades/atividades_n3/Atividade_n3_1.java
@@ -0,0 +1,14 @@
+package atividades.atividade_n3;
+
+import java.util.Scanner;
+
+public class Atividade_n3_1 {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        String nome;
+
+        System.out.println("Digite o seu nome: ");
+        nome = scanner.next();
+        System.out.println("Boas vindas! "+ nome);
+    }
+}

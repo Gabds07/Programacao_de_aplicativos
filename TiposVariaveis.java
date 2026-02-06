f9310bac3e22b15b3567150dc38ab6eee796e827
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-02-06 15:52:59 -0300

    Add TipoVariaveis class with variable examples

---
+++ TiposVariaveis.java
@@ -0,0 +1,18 @@
+public class TipoVariaveis {
+    public static void main() {
+        int idade = 25;
+        System.out.println("A idade é:  "+ idade);
+
+        double altura = 1.75;
+        System.out.println("A altura é: "+ altura);
+
+        boolean isEstudante = true;
+        System.out.println("É estudante? "+ isEstudante);
+
+        char genero = 'M';
+        System.out.println("O gênero é: "+ genero);
+
+        String nome = "Gabriel";
+        System.out.println("O nome é: "+ nome);
+    }
+}

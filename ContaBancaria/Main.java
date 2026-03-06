2e084be5fd20bfc1d029e41938c64f712add9c26
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-03-06 13:52:13 -0300

    Add files via upload

---
+++ ContaBancaria/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+
+        conta.depositar(500);
+        System.out.println(conta.getSaldo());
+        conta.sacar(20);
+        System.out.println(conta.getSaldo());
+    }
+}

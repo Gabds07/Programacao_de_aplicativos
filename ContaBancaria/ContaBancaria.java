1f380033c00f3730cc9dc1157bcc77c35ee1cba2
Gabriel Azevedo <gabrielam12102007@gmail.com>
2026-03-06 14:17:15 -0300

    Refactor withdrawal logic to use array for bills

--- ContaBancaria/ContaBancaria.java
+++ ContaBancaria/ContaBancaria.java
@@ -10,7 +10,6 @@ public class ContaBancaria {
     public void depositar(double valor) {
         if(valor > 0) {
             this.saldo += valor;
-            System.out.println("Valor depositado, saldo atual: R$" + this.saldo);
         } else {
             System.out.println("Não foi possível realizar depósito. Valor menor ou igual a zero.");
         }
@@ -21,20 +20,16 @@ public class ContaBancaria {
         if(valor <= this.saldo) {
             this.saldo -= valor;
 
-            int cedulas_100 = (int)valor / 100;
-            int cedulas_50 = ((int)valor % 100) / 50;
-            int cedulas_20 = ((int)valor % 50) / 20;
-            int cedulas_10 = ((int)valor % 50) / 10;
-            int cedulas_5 = ((int)valor % 10) / 5;
-            int cedulas_1 = ((int)valor % 5);
+            int cedulas[] = { (int)valor / 100, ((int)valor % 100) / 50, ((int)valor % 50) / 20, ((int)valor % 20) / 10, ((int)valor % 10) / 5, ((int)valor % 5) / 2, ((int)valor % 2) };
 
             System.out.println("Você terá que usar: " 
-            + cedulas_100 + " cédulas de R$100, " 
-            + cedulas_50 + " cédulas de R$50, "
-            + cedulas_20 + " cédulas de R$20, "
-            + cedulas_10 + " cédulas de R$10, " 
-            + cedulas_5 + " cédulas de R$5, " 
-            + cedulas_1 + " cédulas de R$1.");
+            + cedulas[0] + " cédulas de R$100, " 
+            + cedulas[1] + " cédulas de R$50, "
+            + cedulas[2] + " cédulas de R$20, "
+            + cedulas[3] + " cédulas de R$10, " 
+            + cedulas[4] + " cédulas de R$5, "
+            + cedulas[5] + " cédulas de R$2, "
+            + cedulas[6] + " cédulas de R$1.");
         } else {
             System.out.println("Não foi possível realizar saque, valor maior que o disponível no saldo da conta.");
         }
